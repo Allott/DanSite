a5aed80d9b7ba9233377c2e116efa1ea22836479
Daniel Allott <36487931+Allott@users.noreply.github.com>
2023-05-11 16:45:16 +0100

    quick skills fix

--- src/components/pages/faces/Buzz/index.js
+++ src/components/pages/faces/Buzz/index.js
@@ -1,34 +1,22 @@
 import { H1, H2 } from '../../../Text'
 
 const items = ['Javascript', 'Typescript', 'React', 'HTML', 'Jest', 'Next', 'Agile', 'CI/CD', 'Tailwind ', 'Accessibility ', 'Adaptive design', 'Bootstrap', 'DOM  ', 'CSS', 'Frontend', 'Github ', 'C# '];
-const wiggle = 200;
 
-const Buzz = ({contactPage}) => {
-    
-    const left = `${wiggle+40}px`
+const Buzz = () => {
     return (
-    <div className='absolute' style={{left: left}}>
-        {items.map((item) => {
-            const value = [...item].reduce((acc, curr) => acc + curr.charCodeAt(0), 0);
-            const x = Math.round(Math.sin(value) * wiggle) + wiggle;
-            const y = Math.round(Math.cos(value) * wiggle) + wiggle;
-            const right = `${x}px`;
-            const top = `${y}px`;
-            return <div
-                        key={item}
-                        style={{right: right, top: top}}
-                        className='absolute'
-                    >
-                    <H2>
-                        {item}
-                    </H2>
-                </div>
-        })}
-        <div className='absolute' style={{right: '180px', top: '200px'}}>
-            <H1 className='w-max'>Core Skills</H1>
+        <div className='flex flex-col items-center'>
+            <H1>Core Skills</H1>
+            <div className='flex flex-row flex-wrap content-between mt-8 w-80'>
+                    {items.map((item) => {
+                        return <div key={item} className='p-2'>
+                                <H2>
+                                    {item}
+                                </H2>
+                        </div>
+                    })}
+            </div>
         </div>
-       
-    </div>
+   
 )};
 
 export default Buzz;
